d0fdef97ad88da9968fa27bd2fd22c5299edd939
Haron Silva Costa <haronsilva@Harons-MacBook-Pro.local>
2023-01-26 22:12:21 +0100

    first commit

---
+++ src/test/java/com/hsc/financial/FinancialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsc.financial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
